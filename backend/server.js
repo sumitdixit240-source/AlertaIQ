f73f3a7dd0dd5dea7152343c9ca34343f93904cf
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-20 20:59:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
-const app = require("./app");
+const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"));
+const authRoutes = require("./routes/auth");
+const alertRoutes = require("./routes/alerts");
 
-app.listen(5000, ()=>console.log("Server running"));
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.use("/api/auth", authRoutes);
+app.use("/api/alerts", alertRoutes);
+
+// DB CONNECT
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// SERVER
+app.listen(5000, () => console.log("Server running on port 5000"));
