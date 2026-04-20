50305fdd4d765e939b36f9d77732782309099d05
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-20 22:40:11 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-const express = require("express");
-const cors = require("cors");
+const API = "http://localhost:5000/api";
 
-const auth = require("./routes/auth");
-const alerts = require("./routes/alerts");
-const payments = require("./routes/payments");
+// LOGIN
+async function login(){
+  const res = await fetch(API+"/auth/login",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+      email:email.value,
+      password:password.value
+    })
+  });
 
-const app = express();
+  const data = await res.json();
 
-app.use(cors());
-app.use(express.json());
+  localStorage.setItem("token",data.token);
 
-app.use("/api/auth", auth);
-app.use("/api/alerts", alerts);
-app.use("/api/payments", payments);
+  alert("Login Success");
+  location.href="dashboard.html";
+}
 
-module.exports = app;
+// CREATE ALERT
+async function createAlert(){
+  const res = await fetch(API+"/alerts",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      "Authorization":localStorage.getItem("token")
+    },
+    body:JSON.stringify({
+      title:title.value,
+      amount:amount.value,
+      expiry:expiry.value
+    })
+  });
+
+  const data = await res.json();
+
+  if(data.upgrade){
+    alert("Upgrade required!");
+  } else {
+    alert("Alert created");
+  }
+}
