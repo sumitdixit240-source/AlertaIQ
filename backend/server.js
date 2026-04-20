26fde9ddc0a276e80bb4cba79db2facf21044aa1
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-21 00:57:50 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authRoutes = require("./routes/auth");
 require("dotenv").config();
 
 const app = express();
