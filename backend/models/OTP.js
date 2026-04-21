1b88ad13adaf9b1a167bb01606dd11d484cbdb42
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-22 01:01:22 +0530

    Update OTP.js

--- backend/models/OTP.js
+++ backend/models/OTP.js
@@ -1,9 +1,13 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const otpSchema = new mongoose.Schema({
   email: String,
   otp: String,
-  createdAt: { type: Date, default: Date.now, expires: 300 }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 300
+  }
 });
 
-export default mongoose.model("OTP", otpSchema);
+module.exports = mongoose.model("OTP", otpSchema);
