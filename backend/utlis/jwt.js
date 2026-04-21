c5a220d39ec003611cb8b6b7fbb2484940583fd0
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-21 15:53:32 +0530

    Update jwt.js

--- backend/utlis/jwt.js
+++ backend/utlis/jwt.js
@@ -1,11 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (user) => {
-  return jwt.sign(
-    { id: user._id, email: user.email },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 };
 
 module.exports = generateToken;
