fd7d0a4d29ee7687be4d24e021891d8967728c54
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-21 15:39:07 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const OTP = require("../models/OTP");
+const OTP = require("./models/OTP");
 const nodemailer = require("nodemailer");
 
 function generateOTP() {
