bd3b81bd9751afbdea0c4602826593e529c7c7de
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-22 21:19:07 +0530

    Update Alert.js

--- backend/models/Alert.js
+++ backend/models/Alert.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const alertSchema = new mongoose.Schema({
   userId: String,
+  email: String, // ✅ add this
   category: String,
   subCategory: String,
   amount: Number,
   expiry: Date,
   frequency: String,
+  lastSent: Date, // ✅ prevent duplicate emails
   createdAt: { type: Date, default: Date.now }
 });
 
