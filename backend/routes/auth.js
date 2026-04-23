8af39aa082fc0f1fa069e0c6c01a7d3a07fc2a59
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-23 16:07:47 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
+const auth = require("../middleware/auth");
 const OTP = require("../models/OTP");
 const sendMail = require("../services/mailer");
 const generateOTP = require("../utils/generateOTP");
